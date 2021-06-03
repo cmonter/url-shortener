17c052e4b66d328c7eb44d0197f719a2c35e229c
Fredomontero <christianmontero08@gmail.com>
2021-06-02 22:47:45 -0700

    Add UI and styles

---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+console.log('Hola Mundo')
+
+const inputURL = document.getElementById('inputURL');
+const btn = document.getElementById('btn');
+const shortURL = document.getElementById('shortURL');
