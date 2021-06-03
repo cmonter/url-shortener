5c53a32f3bf13e1e62ff43713edc3d72e527c18e
Fredomontero <christianmontero08@gmail.com>
2021-06-02 20:45:10 -0700

    Integrated mongodb and mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const generateshortUrL = require('./utils');
+const DB_URL = 'mongodb://127.0.0.1:4444/shortlinks';
 
 const app = express();
 const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo!');
+app.get('/shorturl', (req, res) => {
+  console.log(req.query.url);
+  let shortUrl = generateshortUrL();
+  res.send(shortUrl);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server Running in http://localhost:${PORT}`);
-});
+mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then( () => {
+  console.log("Connected to DB");
+  app.listen(PORT, () => {
+    console.log(`Server Running in http://localhost:${PORT}`);
+  });
+})
+.catch(error => res.send(error))
+
+
+//shorturl?url=https://google.com
